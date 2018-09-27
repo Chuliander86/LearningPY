dec3b3f776d3dfe56813ab126de833bfc48b09f7
chuliander86 <jonathan.quiros@gmail.com>
2018-09-27 09:38:17 +0200

    Primera app

---
+++ apps/01_hello_world/you_try/Hello-world.py
@@ -0,0 +1,5 @@
+print('------------------------------------')
+print('              HELLO WORLD')
+print('------------------------------------')
+name = input('What is your name? ')
+print('Nice to meet you ' + name)
